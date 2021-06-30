27b547cb59783937d53baec8c504ec5ef04e903d
AlekseyG <gelvides.aleksey@outlook.com>
2021-06-30 08:07:09 +0700

    Проблема была в модификатору доступа на конструкторе

--- src/Vector.java
+++ src/Vector.java
@@ -2,7 +2,7 @@ public abstract class Vector {
 
     int x1, y1, z1, x2, y2, z2;
 
-    Vector(int x1, int  y1, int  z1, int  x2, int  y2, int  z2){
+    public Vector(int x1, int  y1, int  z1, int  x2, int  y2, int  z2){
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,5 +1,5 @@
 public class Vector2D extends Vector{
-    Vector2D(int x1, int y1, int x2, int y2) {
+    public Vector2D(int x1, int y1, int x2, int y2) {
         super(x1, y1, 0, x2, y2, 0);
     }
 
